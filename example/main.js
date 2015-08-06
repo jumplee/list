71da3799d5f2e1b413a095a11412f246262ea699
jumplee <liyuntao09@gmail.com>
2015-08-06 09:59:00 +0800

    初始化成功

---
+++ example/main.js
@@ -0,0 +1,13 @@
+var t_table= Handlebars.compile($("#list_tpl").html());
+var url='http://127.0.0.1:8360/home/index/indexJSON';
+
+var options={
+	url:url,
+	dataName:"data",
+	totalName:"total",
+	rowLimit:10,
+	template:t_table,
+	target:"#list"
+	//height:"500px"
+};
+var pp=new List(options);
